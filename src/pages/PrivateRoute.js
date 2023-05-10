3fafc7dc2ebeedd97787847c75db1c565e0ce5cf
Benedict <Benedictnnaoma0@gmail.com>
2023-05-10 22:27:30 +0100

    test

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -3,6 +3,8 @@ import { Navigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const PrivateRoute = ({ children }) => {
+
+  // user api
   const { user } = useAuth0();
   if (!user) {
     return <Navigate to='/' />;
